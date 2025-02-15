67d2b04877f4e871eddad7c0c7e2eec25513f351
Przemek Świderski <118487036+Przemyslaw-Swiderski@users.noreply.github.com>
2025-02-15 16:32:27 +0100

    [TICKET-1][Sprint-1] Initialize Spring Boot project for price microservice. (#SC)(#1)

---
+++ src/test/java/ps/example/pimsprices/PimsPricesApplicationTest.java
@@ -0,0 +1,15 @@
+package ps.example.pimsprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PimsPricesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
