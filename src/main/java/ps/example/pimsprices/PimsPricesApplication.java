17b69bcbed8b6cdc5f131c179574da7dfe515e9e
Przemyslaw-Swiderski <przemyslaw.rafal.swiderski@gmail.com>
2025-02-15 16:14:53 +0100

    [TICKET-1][Sprint-1] Initialize Spring Boot project for price microservice.

---
+++ src/main/java/ps/example/pimsprices/PimsPricesApplication.java
@@ -0,0 +1,11 @@
+package ps.example.pimsprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimsPricesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PimsPricesApplication.class, args);
+    }
+}
