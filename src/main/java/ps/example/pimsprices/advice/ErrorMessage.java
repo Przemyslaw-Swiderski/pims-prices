6de2214d8845a52c2bbd6dd6231d8e032eda740d
Przemek Świderski <118487036+Przemyslaw-Swiderski@users.noreply.github.com>
2025-02-18 01:41:27 +0100

    [TICKET-8][Sprint-6] Add JavaDoc and perform refactoring. (#PRSC)(#15)

--- src/main/java/ps/example/pimsprices/advice/ErrorMessage.java
+++ src/main/java/ps/example/pimsprices/advice/ErrorMessage.java
@@ -4,17 +4,28 @@ import lombok.Data;
 
 import java.util.Date;
 
+/**
+ * Standardized error response model used in exception handling.
+ */
 @Data
 public class ErrorMessage {
-  private int statusCode;
-  private Date timestamp;
-  private String message;
-  private String description;
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
 
-  public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
-    this.statusCode = statusCode;
-    this.timestamp = timestamp;
-    this.message = message;
-    this.description = description;
-  }
+    /**
+     * Constructs an ErrorMessage instance with given details.
+     *
+     * @param statusCode  HTTP status code
+     * @param timestamp   Timestamp of the error occurrence
+     * @param message     Error message
+     * @param description Additional details about the error
+     */
+    public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
+        this.statusCode = statusCode;
+        this.timestamp = timestamp;
+        this.message = message;
+        this.description = description;
+    }
 }
